757df0f7c14395f948eb9c49bc8023bb334936c9
DAM_011 <nadia.elkhalouqi@gmail.com>
2023-10-23 16:27:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/app_template/a01_layout/L01_LinearActivity.java
@@ -0,0 +1,17 @@
+package com.example.app_template.a01_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.app_template.R;
+
+
+
+public class L01_LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l01_linear);
+    }
+}
---
+++ app/src/main/java/com/example/app_template/a03_animation/A01_AnimationLoadingActivity.java
@@ -0,0 +1,16 @@
+package com.example.app_template.a03_animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_template.R;
+
+public class A01_AnimationLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a01_animation_loading);
+    }
+}
